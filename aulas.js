c7be7614b66381847a4561bd597a87a7d06e4f8c
higorlopespro <90470606+higorlopespro@users.noreply.github.com>
2021-09-11 12:07:00 -0300

    Add files via upload

---
+++ aulas.js
@@ -0,0 +1,26 @@
+function aula01(){
+    var nova = window.document.getElementById("video")
+    var titulo = window.document.getElementById("titulo")
+    var desc = window.document.getElementById("desc")
+    titulo.innerText = ("Aula 01 - Introdução ao design")
+    nova.innerHTML = ("<video src=vídeos/01.mp4 controls poster='imagens/0 (2).png'></video>")
+    desc.innerText = ("Você está assistindo a aula de número 01")
+}
+
+function aula02(){
+    var nova = window.document.getElementById("video")
+    var titulo = window.document.getElementById("titulo")
+    var desc = window.document.getElementById("desc")
+    titulo.innerText = ("Aula 02 - Entendendo um pouco mais")
+    nova.innerHTML = ("<video src=vídeos/02.mp4 controls poster='imagens/0 (3).png'></video>")
+    desc.innerText = ("Você está assistindo a aula de número 02")
+}
+
+function aula03(){
+    var nova = window.document.getElementById("video")
+    var titulo = window.document.getElementById("titulo")
+    var desc = window.document.getElementById("desc")
+    titulo.innerText = ("Aula 03 - Dando seus primeiros passos")
+    nova.innerHTML = ("<video src=vídeos/03.mp4 controls poster='imagens/0 (4).png'></video>")
+    desc.innerText = ("Você está assistindo a aula de número 03")
+}
